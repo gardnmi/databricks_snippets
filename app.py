07d3227cac1192d5f8b30f85033bf6e0d6e0dd34
Harjeet Harpal <harjeet@squareup.com>
2023-03-15 09:58:42 +0100

    Use `/api/2.0/workspace/export`
    And other downstream changes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, escape, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from elasticsearch import Elasticsearch, helpers
 from retry import retry
-import json
 import time
 import os
-from scrape import get_notebook_data, get_cell_data
+import traceback
+from scrape import get_notebook_data, export_all_notebooks
+
 
 app = Flask(__name__)
 
@@ -16,19 +17,18 @@ def get_elasticsearch():
 
 @app.route("/check_progress", methods=["GET"])
 def check_progress():
-
     try:
         finished_file_size = os.stat("finished_notebooks.txt").st_size != 0
-        notebook_data_file_size = os.stat("notebook_data.json").st_size != 0
+        all_notebooks_file_size = os.stat("all_notebooks.txt").st_size != 0
     except:
         finished_file_size = None
-        notebook_data_file_size = None
+        all_notebooks_file_size = None
 
-    if finished_file_size and notebook_data_file_size:
+    if finished_file_size and all_notebooks_file_size:
 
-        with open("notebook_data.json", "r") as file:
-            notebook_data = json.load(file)
-            number_of_total_notebooks = len(notebook_data["object_id"])
+        with open("all_notebooks.txt", "r") as file:
+            content = file.read().split("\n")
+            number_of_total_notebooks = len(content)
 
         with open("finished_notebooks.txt", "r") as file:
             content = file.read().split("\n")
@@ -56,71 +56,44 @@ def check_progress():
 
 @app.route("/get_workspace", methods=["GET"])
 def get_workspace():
-
     try:
         workspace = request.args.get("workspace")
+        if workspace == "":
+            workspace = "/Users"
 
         notebook_data = get_notebook_data(workspace=workspace)
-        cell_data = get_cell_data(notebook_data)
-
-        with open("data.json", "w") as outfile:
-            json.dump(cell_data, outfile)
+        notebooks_content = export_all_notebooks(notebook_data)
 
         # Inialize elasticsearch with data
-        with open("data.json") as file:
-            json_data = json.loads(file.read())
-
-        keep_cols = [
-            "id",
-            "command",
-            "pathName",
-            "notebook_id",
-            "notebook_language",
-            "notebook_url",
-            "code_block_url",
-            "code_block_language",
-        ]
-
-        json_data = [{key: data[key] for key in keep_cols}
-                     for data in json_data]
-
         actions = [
             {
                 "_index": "nodes_bulk",
                 "_type": "external",
-                "_id": str(data["id"]),
+                "_id": str(data["notebook_id"]),
                 "_source": data,
             }
-            for data in json_data
+            for data in notebooks_content
         ]
 
         es = get_elasticsearch()
         helpers.bulk(es, actions)
 
-        # clear out files for next run
-        open("finished_notebooks.txt", "w").close()
-        open("notebook_data.json", "w").close()
-
         return jsonify(
             success="Notebooks are now searchable! Please Refresh the page before importing another workspace."
         )
+    
     except Exception as e:
-        # clear out files for next run
-        open("finished_notebooks.txt", "w").close()
-        open("notebook_data.json", "w").close()
-
-        return jsonify(error=f"""Oops...something went wrong: {e}""")
+        return jsonify(error=f"""Oops...something went wrong: {e}\n{traceback.print_exc()}""")
 
 
 @app.route("/", methods=["GET"])
 def find_code_blocks(code_blocks=None):
-
     search = request.args.get("search")
     if search:
         es = get_elasticsearch()
         results = es.search(
             index="nodes_bulk",
-            body={"query": {"match": {"command": search}}},
+            body={"query": {"match": {"notebook_content": search}}},
             size=50,
         )
         code_blocks = results.raw["hits"]["hits"]
--- scrape.py
+++ scrape.py
@@ -2,18 +2,20 @@ import requests
 import json
 from collections import defaultdict
 from dotenv import dotenv_values
+import base64
+
 
 config = dotenv_values(".env")
 
 
 def get_notebook_data(
-    workspace,
-    databricks_url=config["DATABRICKS_URL"],
-    token=config["DATABRICKS_TOKEN"],
+    workspace: str,
+    databricks_url: str = config["DATABRICKS_URL"],
+    token: str = config["DATABRICKS_TOKEN"],
 ):
-
     api_url = "/api/2.0/workspace/list"
     headers = {"Authorization": f"Bearer {token}"}
+
     api_data = defaultdict(list)
 
     def get_api_data(workspace):
@@ -39,86 +41,44 @@ def get_notebook_data(
 
     get_api_data(workspace)
 
-    with open("notebook_data.json", "w") as outfile:
-        json.dump(api_data, outfile)
-
+    with open("all_notebooks.txt", "w") as f:
+        f.write('\n'.join([str(id) for id in api_data["object_id"]]))
+    
     return api_data
 
 
-def get_cell_data(notebook_data):
+def export_all_notebooks(
+    notebook_data,
+    databricks_url: str = config["DATABRICKS_URL"],
+    token: str = config["DATABRICKS_TOKEN"],
+):
+    # Create or overwrite file
+    open("finished_notebooks.txt", "w").close()
 
     code_blocks = []
 
-    # erase file conents
-    open("finished_notebooks.txt", "w").close()
+    api_url = "/api/2.0/workspace/export"
+    headers = {"Authorization": f"Bearer {token}"}  
 
-    headers = {
-        "authority": f"{config['DATABRICKS_URL']}",
-        "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="96", "Microsoft Edge";v="96"',
-        "accept": "*/*",
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "x-requested-with": "XMLHttpRequest",
-        "sec-ch-ua-mobile": "?0",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.43",
-        "sec-ch-ua-platform": '"Windows"',
-        "origin": f"{config['DATABRICKS_URL']}",
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-dest": "empty",
-        "referer": f"{config['DATABRICKS_URL']}/login.html",
-        "accept-language": "en-US,en;q=0.9",
-    }
+    for id, language, path in zip(notebook_data["object_id"], notebook_data["language"], notebook_data["path"]):
 
-    data = {
-        "j_username": config["DATABRICKS_USERNAME"],
-        "j_password": config["DATABRICKS_PASSWORD"],
-    }
+        data = {"path": path}
+        response = requests.get(
+            f"{databricks_url}{api_url}", headers=headers, json=data
+        )
 
-    for id, language in zip(notebook_data["object_id"], notebook_data["language"]):
+        notebook_url = f"{databricks_url}/#notebook/{id}"
+        notebook_content = base64.b64decode(response.json().get('content')).decode('utf-8')
 
-        with requests.Session() as s:
-            login_url = f"{config['DATABRICKS_URL']}/j_security_check"
-            notebook_url = f"{config['DATABRICKS_URL']}/notebook/{id}/command"
+        code_blocks.append({
+            "notebook_path": path,
+            "notebook_id": id,
+            "notebook_language": language,
+            "notebook_url": notebook_url,
+            "notebook_content": notebook_content,
+        })
 
-            login = s.post(login_url, headers=headers, data=data)
-            headers["x-databricks-org-id"] = login.headers["x-databricks-org-id"]
-
-            json_data = s.get(
-                f"{config['DATABRICKS_URL']}/config",
-                headers=headers,
-                cookies=s.cookies,
-            )
-
-            headers["x-csrf-token"] = json_data.json()["csrfToken"]
-
-            json_data = s.get(notebook_url, headers=headers,
-                              cookies=s.cookies).json()
-
-            # Add the ?o paramater otherwise you get csfr errors when accessing link
-            notebook_url = f"{config['DATABRICKS_URL']}/?o={config['DATABRICKS_OID']}#notebook/{id}/command"
-
-            for code_block in json_data:
-                code_block["notebook_id"] = id
-                code_block["notebook_language"] = language
-                code_block["notebook_url"] = notebook_url
-                code_block["code_block_url"] = f"{notebook_url}/{code_block['id']}"
-
-                if "%python" in code_block["command"]:
-                    code_block["code_block_language"] = "python"
-                elif "%scala" in code_block["command"]:
-                    code_block["code_block_language"] = "scala"
-                elif "%sql" in code_block["command"]:
-                    code_block["code_block_language"] = "sql"
-                elif "%md" in code_block["command"]:
-                    code_block["code_block_language"] = "markdown"
-                else:
-                    code_block["code_block_language"] = language.lower()
-
-            code_blocks += json_data
-
-        with open("finished_notebooks.txt", "a") as file:
-            file.write(f"{id}\n")
-
-        print(notebook_url)
-
+        with open("finished_notebooks.txt", "a") as f:
+            f.write(f"{id}\n")
+    
     return code_blocks
